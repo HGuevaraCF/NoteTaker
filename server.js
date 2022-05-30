3981d747928a7404b3782764a10e6f2519efe85d
HGuevaraCF <87338085+HGuevaraCF@users.noreply.github.com>
2022-05-29 22:38:04 -0500

    port change

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const path = require('path');
 const util = require('util');
 const uuid = require('./helpers/uuid');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3000;
+// .listen(process.env.PORT || 5000);
 
 const app = express();
 
